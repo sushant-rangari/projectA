2075f1f71301b0033541d29f2acb0d6c0399753a
sushant-rangari <90927264+sushant-rangari@users.noreply.github.com>
2023-01-22 23:41:28 +0530

    new file added

---
+++ src/com/practice/MyContactInfo.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class MyContactInfo {
+
+	public static void main(String[] args) {
+		
+    System.out.println("Line from master branch");
+   
+    System.out.println("some new line changes"); 
+ 
+
+	}
+
+}
