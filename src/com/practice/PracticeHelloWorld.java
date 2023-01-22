380e3a716799480f426418e7d940709c18cf92a5
sushant-rangari <ntechsushant@yahoo.co.in>
2023-01-22 23:17:04 +0530

    added a line from feature branch

--- src/com/practice/PracticeHelloWorld.java
+++ src/com/practice/PracticeHelloWorld.java
@@ -5,6 +5,8 @@ public class PracticeHelloWorld {
 	public static void main(String[] args) {
 
 		System.out.println("Hello World Practice Project A");
+		
+		System.out.println("Added line from feature branch");
 
 	}
 
